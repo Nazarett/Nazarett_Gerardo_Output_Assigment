f1e37c794993153630479d917b33be3679e62883
Nazarett <gnazarett@gmail.com>
2015-06-07 21:33:26 -0400

    Number Variable commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Gerardo D Nazarett
+//C201506
+//Data Infrastructers
+//Creating Variables And Outputs
+//Number Variable
+
+var age = 36;
+var dayBorn = 30;
+var monthBorn = 03;
+var yearBorn = 1979;
+console.log("This will let you know my age, day, month and year I was born");
+console.log(age)
+console.log(dayBorn);
+console.log(monthBorn);
+console.log(yearBorn);
